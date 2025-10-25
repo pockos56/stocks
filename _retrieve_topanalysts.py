b8f0d4589156e0c96bc0ed33673f867fdc6584e6
pockos56 <103532988+pockos56@users.noreply.github.com>
2025-10-25 13:31:55 +0200

    Fixed " -> '

--- _retrieve_topanalysts.py
+++ _retrieve_topanalysts.py
@@ -127,7 +127,7 @@ def analyst_ranks(end_number=NO_ANALYSTS_IN_MARKETBEAT, manual_list=manual_list,
 
     # Optionally save
     if save:
-        file_dir_timestamp = os.path.join('data', f'{datetime.now().strftime('%Y%m%d')}_{mode}_top_analysts.csv')
+        file_dir_timestamp = os.path.join('data', f"{datetime.now().strftime('%Y%m%d')}_{mode}_top_analysts.csv")
         analyst_data.to_csv(file_dir_timestamp, index=False)
         file_dir_latest = os.path.join('data', 'latest_top_analysts.csv')
         analyst_data.to_csv(file_dir_latest, index=False)
